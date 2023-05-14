0519f7ae88571ff6b1370943e38225a44a99bf47
EduardoGDM <eduardogdiasmachado@gmail.com>
2023-05-13 21:30:16 -0300

    first deploy

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -9,26 +9,29 @@ const Carousel = () => {
     },[count])
     return (
         <Fragment>
-            <div className="font-Maxwell flex flex-col justify-between  md:flex-row p-20 ">   
+            <div className="font-Maxwell flex flex-col justify-between  md:flex-row p-20 ">  
+             <span className="text-sm">Para continuar,clique em Realizar Pedido</span>
               <div className="p-5">
                 <figure className="relative max-w-sm hover:scale-110 rounded-lg shadow-xl dark:shadow-gray-900 cursor-pointer filter">
                 <a href="#">
-                    <img className="rounded-lg object-cover " src="Palha.jpg" alt="image description"/>
+                    <img className="rounded-lg object-cover" src="Palha.jpg" alt="image description"/>
                 </a>
                 <figcaption className="absolute rounded-lg px-4 text-lg text-white  bg-[#633112]   bottom-4">
                 
                 </figcaption>
                 </figure>
-                
-         </div> 
-         <div className="flex p-[9%]">
+
+                <div className="flex p-[9%]">
       <button disabled = { count < 1 ? true : false} onClick={()=> setCount(count - 1 )} className="bg-white hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-full">-</button>
       <span className="p-2"> Unidades: {count}</span>
          <button onClick={()=> setCount(count + 1 )} className="bg-white hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-full">+</button>
         
     </div>
     
-    <span>Para continuar,clique em Realizar Pedido</span>
+                
+         </div> 
+        
+    
 
          <div className="p-5">
                 <figure className="relative max-w-sm hover:scale-110 rounded-lg shadow-xl dark:shadow-gray-800 cursor-pointer filter">
@@ -46,7 +49,7 @@ const Carousel = () => {
                 <a href="#">
                     <img className="rounded-lg  " src="https://cdn.awsli.com.br/600x450/203/203149/produto/29270318/6ecdd64df7.jpg" alt="image description"/>
                 </a>
-                <figcaption className="absolute rounded-lg px-4 text-lg text-white bg-[#633112]     bottom-4">
+                <figcaption className="absolute rounded-lg px-4 text-lg text-white bg-[#633112] bottom-4">
                     <p className="scale-[%50]">Bolo de Cenoura</p>
                 </figcaption>
                 </figure>
